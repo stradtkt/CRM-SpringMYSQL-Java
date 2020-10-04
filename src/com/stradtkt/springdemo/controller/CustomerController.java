ced19eb621718c42b1087cee7cc53da590020d9b
Kevin Stradtman <stradtkt22@gmail.com>
2020-10-04 13:16:30 -0400

    Added a service

--- src/com/stradtkt/springdemo/controller/CustomerController.java
+++ src/com/stradtkt/springdemo/controller/CustomerController.java
@@ -5,21 +5,23 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.stradtkt.springdemo.dao.CustomerDAO;
 import com.stradtkt.springdemo.entity.Customer;
+import com.stradtkt.springdemo.service.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
 	@Autowired
-	private CustomerDAO customerDAO;
-
-	@RequestMapping("/list")
+	private CustomerService customerService;
+	
+	@GetMapping("/list")
 	public String listCustomers(Model theModel) {
-		List<Customer> theCustomers = customerDAO.getCustomers();
+		List<Customer> theCustomers = customerService.getCustomers();
 		theModel.addAttribute("customers", theCustomers);
 		return "list-customers";
 	}
--- src/com/stradtkt/springdemo/dao/CustomerDAOImpl.java
+++ src/com/stradtkt/springdemo/dao/CustomerDAOImpl.java
@@ -18,7 +18,6 @@ public class CustomerDAOImpl implements CustomerDAO {
 	private SessionFactory sessionFactory;
 	
 	@Override
-	@Transactional
 	public List<Customer> getCustomers() {
 		Session currentSession = sessionFactory.getCurrentSession();
 		Query<Customer> theQuery = currentSession.createQuery("from Customer", Customer.class);
