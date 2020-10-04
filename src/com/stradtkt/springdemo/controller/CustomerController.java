2f474ac04c958dbe3a53bfed17dcb8efc54355c2
Kevin Stradtman <stradtkt22@gmail.com>
2020-10-04 08:46:03 -0400

    Added more JAR files and set up the database connection

--- src/com/stradtkt/springdemo/controller/CustomerController.java
+++ src/com/stradtkt/springdemo/controller/CustomerController.java
@@ -1,4 +1,4 @@
-package com.luv2code.springdemo.controller;
+package com.stradtkt.springdemo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- src/com/stradtkt/testdb/TestDbServlet.java
+++ src/com/stradtkt/testdb/TestDbServlet.java
@@ -1,4 +1,4 @@
-package com.luv2code.testdb;
+package com.stradtkt.testdb;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -11,39 +11,23 @@ import javax.servlet.http.HttpServletResponse;
 
 import java.sql.*;
 
-/**
- * Servlet implementation class TestDbServlet
- */
 @WebServlet("/TestDbServlet")
 public class TestDbServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
 		// setup connection variables
 		String user = "springstudent";
 		String pass = "springstudent";
-		
 		String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false&serverTimezone=UTC";
 		String driver = "com.mysql.cj.jdbc.Driver";
-		
 		// get connection to database
 		try {
 			PrintWriter out = response.getWriter();
-			
 			out.println("Connecting to database: " + jdbcUrl);
-			
 			Class.forName(driver);
-			
 			Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
-			
 			out.println("SUCCESS!!!");
-			
 			myConn.close();
-			
 		}
 		catch (Exception exc) {
 			exc.printStackTrace();
