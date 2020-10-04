1b7c2c8e55ecb6e74e43d2db17f2cf83862bd373
Kevin Stradtman <stradtkt22@gmail.com>
2020-10-04 11:48:51 -0400

    Ability to show items from the database on the servers web page

--- src/com/stradtkt/springdemo/controller/CustomerController.java
+++ src/com/stradtkt/springdemo/controller/CustomerController.java
@@ -1,16 +1,26 @@
 package com.stradtkt.springdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.stradtkt.springdemo.dao.CustomerDAO;
+import com.stradtkt.springdemo.entity.Customer;
+
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+	
+	@Autowired
+	private CustomerDAO customerDAO;
 
 	@RequestMapping("/list")
 	public String listCustomers(Model theModel) {
-		
+		List<Customer> theCustomers = customerDAO.getCustomers();
+		theModel.addAttribute("customers", theCustomers);
 		return "list-customers";
 	}
 	
